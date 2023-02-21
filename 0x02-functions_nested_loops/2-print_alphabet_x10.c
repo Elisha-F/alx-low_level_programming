24577de1e56ce0350ba4bb75855d9ebeab4e518d
Elisha Misoga <misogafaith5@gmail.com>
2023-02-21 12:13:08 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * Prints 10 times the alphabet
+ *
+ * returns void
+ */
+void print_alphabet_x10(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+return;
+}
