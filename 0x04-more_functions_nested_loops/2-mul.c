726b34a93b6ed20f1512a2714ff7a85379f734c9
Elisha Misoga <misogafaith5@gmail.com>
2023-02-26 13:13:44 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Always multiplcation
+ */
+
+int mul(int a, int b)
+{
+int product = a * b;
+return (product);
+}
