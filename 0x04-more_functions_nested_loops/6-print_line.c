feaaf5369ec61fd83e26ec031c47ac015c0b433a
Elisha Misoga <misogafaith5@gmail.com>
2023-02-26 14:21:03 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - print a line on the terminal
+ * @n: number of times _ appears on the terminal
+ * Return: Number of _ n
+ */
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+if (n < 0)
+{
+_putchar('\n');
+}
+else
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
