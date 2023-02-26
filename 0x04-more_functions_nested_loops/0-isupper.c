57f5edf518c1dba08e23f95ccf9ec4663140a0b9
Elisha Misoga <misogafaith5@gmail.com>
2023-02-26 12:27:29 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checks uppercase character
+ * @c: variable character c
+ * Return: 1 if c is uppercase, and ) otherwise
+ */
+
+int _isupper(int c)
+{
+
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
