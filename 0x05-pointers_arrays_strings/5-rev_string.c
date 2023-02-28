292d7047bd979352a76dc5ed2e844c830883e994
Elisha Misoga <misogafaith5@gmail.com>
2023-02-28 13:23:51 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: variable of type char
+ * Return: 0 Always
+ */
+void rev_string(char *s)
+{
+int i, c, k;
+char *a, aux;
+a = s;
+while (s[c] != '\0')
+{
+c++;
+}
+for (k = 1; k < c; k++)
+{
+a++;
+}
+for (i = 0; i < (c / 2); i++)
+{
+aux = s[i];
+s[i] = *a;
+*a = aux;
+a--;
+}
+}
