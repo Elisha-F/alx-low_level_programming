04fc9b6f3895b4282af990a355478597fa35606c
Elisha Misoga <misogafaith5@gmail.com>
2023-02-28 12:27:52 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - return the length of a string
+ * @s: pointer to the string 
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int len = strlen(s);
+return (len);
+}
+
