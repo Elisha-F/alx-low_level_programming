feb98a70f4544ad3907ce4e0464f08978152c8a4
Elisha Misoga <misogafaith5@gmail.com>
2023-02-28 12:50:37 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - prints a string folowed by a line
+ * @str: string variable
+ * Return: 0 Always
+ */
+void _puts(char *str)
+{
+puts(str);
+}
