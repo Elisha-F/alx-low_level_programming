8ccf394962e6262deb45a1548b166c1e4ee98fe4
Elisha Misoga <misogafaith5@gmail.com>
2023-03-01 11:08:06 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - Concantenates two strings
+ * @dest: char destination
+ * @src: char source
+ *Return: contents of variable dest
+ * /
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
