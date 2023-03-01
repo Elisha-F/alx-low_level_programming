2636b920643a012410bbe2cd1d6e637faf55d656
Elisha Misoga <misogafaith5@gmail.com>
2023-03-01 12:50:30 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
  * @s2: second string
  * Return: returns the comparisons between the two strings
  */
-int _strcmp(scha *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-int i;
+int i = 0;
 while (s1[i] == s2[i])
 {
 if (s1[i] == '\0')
