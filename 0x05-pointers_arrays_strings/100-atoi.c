862bcb9dbedaa9abbd39d3d8f90a3f0a52908438
Elisha Misoga <misogafaith5@gmail.com>
2023-03-01 09:18:42 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _atoi - Converts string to an integer
+ * @s: variable char
+ * Return: 0 if no string, and integer when their is a string
+ */
+int _atoi(char *s)
+{
+int val;
+val = atoi(s);
+return (val);
+}
