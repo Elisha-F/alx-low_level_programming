f6aca6f4e7dd6d79e419e73d4e8d27bdf1d2d519
Elisha Misoga <misogafaith5@gmail.com>
2023-03-01 10:04:33 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/**
+ * main - generates password
+ *
+ * Return: 0 Always
+ */
 #define PASSWORD_LENGTH 12
-
-int main()
+int main(void)
 {
 char password[PASSWORD_LENGTH + 1];
 const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
 const int charset_size = sizeof(charset) - 1;
-srand(time(NULL)); // Initialize random number generator with current time
-for (int i = 0; i < PASSWORD_LENGTH; i++) {
-int random_index = rand() % charset_size; // Generate random index within charset
-password[i] = charset[random_index]; // Add randomly selected character to password
+srand(time(NULL));
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+int random_index = rand() % charset_size;
+password[i] = charset[random_index];
 }
-password[PASSWORD_LENGTH] = '\0'; // Add null terminator at end of password string
+password[PASSWORD_LENGTH] = '\0';
 printf("Generated password: %s\n", password);
-return 0;
+return (0);
 }
 
