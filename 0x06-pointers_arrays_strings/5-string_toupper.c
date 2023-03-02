e356743b7f566ea68a6604c72ee07940003eadb4
Elisha Misoga <misogafaith5@gmail.com>
2023-03-02 09:56:08 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * string_toupper - changes lower case to upper case
+ * @str: char pointer
+ * Return: Uppercase
+ */
+char *string_toupper(char *str)
+{
+long unsigned int i;
+for (i = 0; i < strlen(str); i++)
+{
+if (islower(str[i]))
+{
+str[i] = toupper(str[i]);
+}
+}
+return (str);
+}
