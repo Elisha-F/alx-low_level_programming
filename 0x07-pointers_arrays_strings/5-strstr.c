be04ba6d6c286669e9ebd5e7fd7e0d04c85c26fe
Elisha Misoga <misogafaith5@gmail.com>
2023-03-06 13:49:53 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: Pointer of char type
+ * @needle: pointer of char type
+ * Return: a pointer to the beginning of the located substrings
+ */
+char *_strstr(char *haystack, char *needle)
+{
+char *ret;
+ret = stratr(haystack, needle)
+return (ret);
+}
