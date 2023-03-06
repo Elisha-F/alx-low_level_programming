0a957ab098c0f97d5bb288f7e6faa84dccf44ae5
Elisha Misoga <misogafaith5@gmail.com>
2023-03-06 13:15:01 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <strings.h>
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: Pointer of char type
+ * @accept: pointer of char type
+ * Return: No. of initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int k;
+k = strspn(s, accept)
+return (k);
+}
