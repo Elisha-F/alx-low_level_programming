93d2906f44b512021d9522287fd4fbdd7b678006
Elisha Misoga <misogafaith5@gmail.com>
2023-03-07 11:55:32 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,5 @@ for (i = 0; i < size; i++)
 sum1 += a[i * size + i];
 sum2 += a[i * size + size - 1 - i];
 }
-printf("%d,", sum1);
-printf("%d,", sum2);
-printf("\n");
+printf("%d, %d\n", sum1, sum2);
 }
