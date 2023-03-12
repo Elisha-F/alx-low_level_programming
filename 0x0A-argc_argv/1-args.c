bdd4841d0cadd40632f06f4c1da49bda60e302c6
Elisha Misoga <misogafaith5@gmail.com>
2023-03-12 04:54:01 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: No. of arguments
+ * @argsv: argument vector
+ * Return: 0 always
+ */
+int main(int argc,char *argv[])
+{
+argv[0] = 0;
+printf("%d\n", argc - 1);
+return (0);
+}
