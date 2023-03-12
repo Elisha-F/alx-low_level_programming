9a6916b35cf49f5cefa15cae242dd08f8d51fbbb
Elisha Misoga <misogafaith5@gmail.com>
2023-03-12 06:15:07 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,12 +10,16 @@ int main(int argc, char *argv[])
 {
 int sum = 0;
 int count;
+/*if (argc == 1)
+{
+printf("0\n");
+}*/
+for (count = 1; count < argc; count++)
+{
 if (argc == 1)
 {
 printf("0\n");
 }
-for (count = 1; count < argc; count++)
-{
 if (atoi(argv[count]) >= 0)
 {
 sum += atoi(argv[count]);
