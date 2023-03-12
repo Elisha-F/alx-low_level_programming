1ba05e5ef061aaf4f8315fe14771e15b2b37e0a2
Elisha Misoga <misogafaith5@gmail.com>
2023-03-12 04:41:13 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: int variable
+ * @argv: array of char type
+ * Return: 0 Always
+ */
+
+int main(int  argc, char *argv[])
+{
+argc--;
+printf("%s\n", argv[0]);
+return (0);
+}
