198e9aced5db2c5dfb86cc19e42429912e89a0bf
Elisha Misoga <misogafaith5@gmail.com>
2023-03-14 20:24:32 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/1-strdup_copy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly created space memory
+ * @str: pointer variable
+ * Return: Null if str is NULL,NULL if no memory
+ */
+char *_strdup(char *str)
+{
+int len = strlen(str);
+char *newstr = (char *)malloc((len + 1) * sizeof(char));
+if (str == NULL)
+{
+return (NULL);
+}
+if (newstr == NULL)
+{
+return (NULL);
+}
+strcpy(newstr, str);
+return (newstr);
+free(newstr);
+} /* similar to function strdup()*/
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - concantenates twos strings
+ * @s1: string pointer 1
+ * @s2: string pointer 2
+ * Return: Null of failure, pointer of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+int len1 = strlen(s1);
+int len2 = strlen(s2);
+char *newstr = (char *) malloc((len1 + len2 + 1) * sizeof(char));
+if (newstr == NULL)
+{
+return (NULL);
+}
+strcpy(newstr, s1);
+strcat(newstr, s2);
+return (newstr);
+}
