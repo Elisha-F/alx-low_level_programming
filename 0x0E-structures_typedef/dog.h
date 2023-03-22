ec590001fe3841cfa646f323856e4b7420f42891
Elisha Misoga <misogafaith5@gmail.com>
2023-03-22 06:58:14 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Puppy information
+ * @name: Name of the puppy
+ * @age: age of the puppy
+ * @owner: the owner
+ * Description: The struct highlights the information concerning the puppy
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /*DOG_H*/
