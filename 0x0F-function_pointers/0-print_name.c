595f1acdbcc5f3f9a9f7a2ca1c904d43cdc155df
Elisha Misoga <misogafaith5@gmail.com>
2023-03-26 03:39:55 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints name
+ * @name: name variable
+ * @f: funtion pointer
+ * Return: 0 always
+ */
+void print_name(char *name, void(*f)(char *))
+{
+f = &print_name;
+f(name);
+}
