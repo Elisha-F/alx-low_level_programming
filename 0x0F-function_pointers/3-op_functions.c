92366a204c4a52358438ff193e54655db139c721
Elisha Misoga <misogafaith5@gmail.com>
2023-03-26 06:07:03 -0700

    Simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two numbers
+ * @a: int value one
+ * @b: int value b
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - adds two numbers
+ * @a: int value one
+ * @b: int value b
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - adds two numbers
+ * @a: int value one
+ * @b: int value b
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - adds two numbers
+ * @a: int value one
+ * @b: int value b
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - adds two numbers
+ * @a: int value one
+ * @b: int value b
+ * Return: sum of a and b
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
